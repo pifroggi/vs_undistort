ed25c6aabe1db727b61f3816ab669fcbeeed14da
pifroggi <pifroggimc@gmail.com>
2025-09-08 19:25:46 +0200

    add tile width/height params, faster input/output, fix autocast warning

--- vs_undistort/TMT_dynamic_1st_stage.py
+++ vs_undistort/TMT_dynamic_1st_stage.py
@@ -1,65 +1,54 @@
-
 # Code from "Turbulence Mitigation Transformer" https://github.com/xg416/TMT
 
-# Vapoursynth Implementation by pifroggi
-# or tepete on the "Enhance Everything!" Discord Server
+# Vapoursynth Implementation by pifroggi https://github.com/pifroggi/vs_undistort
+# or tepete and pifroggi on Discord
 
 import torch
-import torch.nn.functional as F
 
-def fetch_images(image_tensor, temp_window, patch_unit=16):
-    h, w, c = image_tensor.shape
-    frame_width = w // temp_window
-    frames = torch.empty((temp_window, c, h, frame_width), dtype=image_tensor.dtype, device=image_tensor.device)
 
-    for i in range(temp_window):
-        frames[i] = image_tensor[:, i * frame_width:(i + 1) * frame_width, :].permute(2, 0, 1)
-
-    #padding
-    padh = patch_unit - h % patch_unit if h % patch_unit != 0 else 0
-    padw = patch_unit - frame_width % patch_unit if frame_width % patch_unit != 0 else 0
-    frames = F.pad(frames, (0, padw, 0, padh), mode='reflect')
-
-    return frames, h, frame_width, temp_window
-
-def split_to_patches(h, w, s):
-    nh = h // s + (1 if h % s != 0 else 0)
-    nw = w // s + (1 if w % s != 0 else 0)
-    ol_h = int((nh * s - h) / (nh - 1)) if nh > 1 else 0
-    ol_w = int((nw * s - w) / (nw - 1)) if nw > 1 else 0
+def split_to_patches(h, w, s_h, s_w):
+    nh = h // s_h + (1 if h % s_h != 0 else 0)
+    nw = w // s_w + (1 if w % s_w != 0 else 0)
+    ol_h = int((nh * s_h - h) / (nh - 1)) if nh > 1 else 0
+    ol_w = int((nw * s_w - w) / (nw - 1)) if nw > 1 else 0
     hpos, wpos = [0], [0]
-    for i in range(1, nh):
-        hpos.append(min(hpos[-1] + s - ol_h, h - s))
-    for i in range(1, nw):
-        wpos.append(min(wpos[-1] + s - ol_w, w - s))
+    for _ in range(1, nh):
+        hpos.append(min(hpos[-1] + s_h - ol_h, h - s_h))
+    for _ in range(1, nw):
+        wpos.append(min(wpos[-1] + s_w - ol_w, w - s_w))
     return hpos, wpos
 
-def test_spatial_overlap(input_blk, model_tilt, patch_size):
-    b, c, l, h, w = input_blk.shape
-    hpos, wpos = split_to_patches(h, w, patch_size)
-    out_spaces = torch.zeros_like(input_blk)
-    out_masks = torch.zeros((b, c, l, h, w), device=input_blk.device, dtype=input_blk.dtype)
-    ones_patch = torch.ones((b, c, l, patch_size, patch_size), device=input_blk.device, dtype=input_blk.dtype)
+def test_spatial_overlap(input_blk, model_tilt, patch_height, patch_width):
+    b, l, c, h, w = input_blk.shape
+    hpos, wpos = split_to_patches(h, w, patch_height, patch_width)
+    out_spaces = torch.zeros_like(input_blk)                                               # (B, L, C, H, W)
+    out_counts = torch.zeros((b, 1, 1, h, w), device=input_blk.device, dtype=torch.int16)  # (B, 1, 1, H, W)
+    ones_count = None
 
     for hi in hpos:
         for wi in wpos:
-            input_ = input_blk[..., hi:hi + patch_size, wi:wi + patch_size]
-            _, _, rectified = model_tilt(input_.permute(0, 2, 1, 3, 4))
-            rectified = rectified.permute(0, 2, 1, 3, 4)
-            out_spaces[..., hi:hi + patch_size, wi:wi + patch_size] += rectified
-            out_masks[..., hi:hi + patch_size, wi:wi + patch_size] += ones_patch[..., :rectified.shape[-2], :rectified.shape[-1]]
+            inp = input_blk[..., hi:hi + patch_height, wi:wi + patch_width]                # (B, L, C, ph, pw)
+            _, _, rectified = model_tilt(inp)
+            hs, ws = rectified.shape[-2:]
+            out_spaces[..., hi:hi + hs, wi:wi + ws].add_(rectified)
+            if ones_count is None or ones_count.shape[-2:] != (hs, ws):
+                ones_count = torch.ones((1, 1, 1, hs, ws), device=input_blk.device, dtype=out_counts.dtype)
+            out_counts[..., hi:hi + hs, wi:wi + ws].add_(ones_count)
 
-    out_masks[out_masks == 0] = 1
-    return out_spaces / out_masks
+    return out_spaces / out_counts
 
-def process_images(image_tensor, patch_size, temp_window, model_tilt):
-    turb_imgs, h, frame_width, _ = fetch_images(image_tensor, temp_window, patch_size)
-    turb_imgs = turb_imgs.unsqueeze(0).permute(0, 2, 1, 3, 4)
 
-    with torch.no_grad():
-        with torch.cuda.amp.autocast():
-            recovered = test_spatial_overlap(turb_imgs, model_tilt, patch_size)
-        recovered = recovered[..., :h, :frame_width].permute(0, 2, 1, 3, 4)
+def process_images(frames_tensor, patch_height, patch_width, model_tilt):
+    T, C, H, W = frames_tensor.shape
+    input_blk  = frames_tensor.unsqueeze(0)                                                # (1, T, C, H, W)
 
-        stacked_image = torch.cat([recovered[0, i].permute(1, 2, 0) for i in range(recovered.shape[1])], dim=1)
-        return stacked_image
+    with torch.inference_mode():
+        if input_blk.device.type == "cuda":
+            with torch.amp.autocast(device_type="cuda"):
+                recovered = test_spatial_overlap(input_blk, model_tilt, patch_height, patch_width)
+        else:
+            recovered = test_spatial_overlap(input_blk, model_tilt, patch_height, patch_width)
+
+    recovered = recovered[0]
+    stacked   = recovered.permute(2, 0, 3, 1).contiguous().reshape(H, T * W, C)  # output one large frame to let vs handle the caching
+    return stacked
