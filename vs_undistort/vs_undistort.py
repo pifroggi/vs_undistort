ddf56d1fb573445fed074c84351d9db3e22b8133
pifroggi <pifroggimc@gmail.com>
2026-06-30 22:25:59 +0200

    add support for vsmlrt with tensorrt 11

--- vs_undistort/vs_undistort.py
+++ vs_undistort/vs_undistort.py
@@ -77,17 +77,20 @@ def _get_engine(onnx_path, engine_dir, temp_window=10, width=720, height=480, fo
     logging.warning("vs_undistort.tensorrt: Building new TensorRT engine for temp_window=%d, tile_width=%d, tile_height=%d. This may take a few minutes.", temp_window, width, height)
     
     # engine in/out shape: (1, temp_window * 3, height, width)
-    trtexec    = _get_trtexec()
-    opt_shapes = f"input:1x{temp_window * 3}x{height}x{width}"
-    opt_level  = 1
+    trtexec     = _get_trtexec()
+    trt_version = int(core.trt.Version()["tensorrt_version"].decode(errors="ignore"))
+    trt_version = [trt_version // 10000, (trt_version % 10000) // 100, trt_version % 100]
+    io_formats  = f"fp16:chw" if trt_version[0] < 11 else "chw"
+    opt_shapes  = f"input:1x{temp_window * 3}x{height}x{width}"
+    opt_level   = 1
     cmd = [
         trtexec,
         "--stronglyTyped",
-        "--inputIOFormats=fp16:chw",
-        "--outputIOFormats=fp16:chw",
         "--skipInference",
         "--avgTiming=3",
         "--memPoolSize=workspace:6144",
+        f"--inputIOFormats={io_formats}",
+        f"--outputIOFormats={io_formats}",
         f"--onnx={onnx_path}",
         f"--saveEngine={engine_path}",
         f"--optShapes={opt_shapes}",
